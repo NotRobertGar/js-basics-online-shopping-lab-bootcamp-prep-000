ccd1862523f7ba4d2b7e400e1c174b5faec74706
cab driver <cocktailsthecomic@gmail.com>
2017-09-15 22:33:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }
   if (cart.length === 1){
-    console.log(`In your cart, you have " ${cart} + "  $${cart[i]}`);
+    console.log(`In your cart, you have " ${cart} + "  $${cart}`);
 
   }
   if (cart.length === 2){
